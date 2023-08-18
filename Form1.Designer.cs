a97f42c94927781916f459f49d8e97ee359bb87d
Ruslan <abishev2012@gmail.com>
2023-08-18 23:17:27 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace ClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServerIp = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServerPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.txtNewTask = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.имя = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServerIp
+            // 
+            this.txtServerIp.Location = new System.Drawing.Point(127, 52);
+            this.txtServerIp.Name = "txtServerIp";
+            this.txtServerIp.Size = new System.Drawing.Size(100, 20);
+            this.txtServerIp.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP";
+            // 
+            // txtServerPort
+            // 
+            this.txtServerPort.Location = new System.Drawing.Point(127, 107);
+            this.txtServerPort.Name = "txtServerPort";
+            this.txtServerPort.Size = new System.Drawing.Size(100, 20);
+            this.txtServerPort.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Порт";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(319, 55);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "label3";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(133, 144);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(103, 23);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Соединение";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click_1);
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.Location = new System.Drawing.Point(40, 252);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(103, 23);
+            this.btnAddTask.TabIndex = 6;
+            this.btnAddTask.Text = "Добавить задачу";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.Location = new System.Drawing.Point(464, 32);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(303, 277);
+            this.lstTasks.TabIndex = 7;
+            // 
+            // txtNewTask
+            // 
+            this.txtNewTask.Location = new System.Drawing.Point(164, 252);
+            this.txtNewTask.Name = "txtNewTask";
+            this.txtNewTask.Size = new System.Drawing.Size(100, 20);
+            this.txtNewTask.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(343, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Новая задача";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(74, 316);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(100, 20);
+            this.txtUsername.TabIndex = 10;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(254, 316);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // имя
+            // 
+            this.имя.AutoSize = true;
+            this.имя.Location = new System.Drawing.Point(22, 322);
+            this.имя.Name = "имя";
+            this.имя.Size = new System.Drawing.Size(29, 13);
+            this.имя.TabIndex = 12;
+            this.имя.Text = "Имя";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(191, 322);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Пароль";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(181, 377);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(144, 23);
+            this.btnLogin.TabIndex = 14;
+            this.btnLogin.Text = "зарегистрировать";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.имя);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNewTask);
+            this.Controls.Add(this.lstTasks);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServerPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtServerIp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServerIp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServerPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.ListBox lstTasks;
+        private System.Windows.Forms.TextBox txtNewTask;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label имя;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientApp
+{
+   
+    public partial class Form1 : Form
+    {
+        [Serializable]
+        public class ClientSettings
+        {
+            public string ServerIp { get; set; }
+            public int ServerPort { get; set; }
+        }
+
+        private TcpClient client;
+        private NetworkStream stream;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Click_1(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                // Создаем клиент и подключаемся к серверу
+                client = new TcpClient();
+
+                try
+                {
+                    string serverIp = txtServerIp.Text;
+                    int serverPort = int.Parse(txtServerPort.Text);
+
+                    await client.ConnectAsync(serverIp, serverPort);
+                    stream = client.GetStream();
+
+                    await AddUser(username, password);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка подключения к серверу: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите имя пользователя и пароль.");
+            }
+        }
+
+        private async Task AddUser(string username, string password)
+        {
+            try
+            {
+                // Отправляем запрос на сервер для регистрации нового пользователя
+                string request = $"REGISTER|{username}|{password}";
+                await SendMessageAsync(request);
+
+                // Читаем ответ от сервера
+                string response = await ReadMessageAsync<string>();
+
+                if (response == "SUCCESS")
+                {
+                    MessageBox.Show("Пользователь успешно зарегистрирован.");
+                }
+                else
+                {
+                    MessageBox.Show("Ошибка регистрации пользователя.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при регистрации пользователя: {ex.Message}");
+            }
+            finally
+            {
+                // Закрываем соединение после завершения операции
+                client.Close();
+            }
+        }
+
+
+
+        private async void btnConnect_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                string serverIp = txtServerIp.Text;
+                int serverPort = int.Parse(txtServerPort.Text);
+
+                // Создаем клиент и подключаемся к серверу
+                client = new TcpClient();
+                await client.ConnectAsync(serverIp, serverPort);
+                stream = client.GetStream();
+                lblStatus.Text = "Подключено к серверу.";
+
+                // Получаем список дел пользователя после успешной авторизации
+                await GetTasks();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка подключения к серверу: {ex.Message}");
+            }
+        }
+
+        private async Task GetTasks()
+        {
+            // Отправляем запрос на получение списка дел
+            await SendMessageAsync("GET_TASKS");
+
+            // Читаем ответ от сервера
+            List<string> tasks = await ReadMessageAsync<List<string>>();
+            lstTasks.Items.Clear();
+            lstTasks.Items.AddRange(tasks.ToArray());
+        }
+
+        private async Task AddTask()
+        {
+            string task = txtNewTask.Text;
+
+            // Отправляем запрос на добавление нового дела
+            await SendMessageAsync("ADD_TASK");
+            await SendMessageAsync(task);
+
+            // Обновляем список дел после добавления нового дела
+            await GetTasks();
+        }
+
+        private async Task SendMessageAsync(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            await stream.WriteAsync(data, 0, data.Length);
+        }
+
+        private async Task<T> ReadMessageAsync<T>()
+        {
+            byte[] buffer = new byte[4096];
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            byte[] messageBytes = new byte[bytesRead];
+            Array.Copy(buffer, messageBytes, bytesRead);
+            return Deserialize<T>(messageBytes);
+        }
+
+        private T Deserialize<T>(byte[] data)
+        {
+            using (var stream = new System.IO.MemoryStream(data))
+            {
+                var formatter = new BinaryFormatter();
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+
+        private async void btnAddTask_Click(object sender, EventArgs e)
+        {
+            await AddTask();
+        }
+
+       
+    }
+}
